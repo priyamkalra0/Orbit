3149b162a4d3a650693601e3ea3550a15ef99592
priyamkalra0 <priyamkalra0@gmail.com>
2025-11-20 21:54:45 +0530

    graphics: switched to 1440p output resolution

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,8 +6,8 @@
 #include "Graphics/World.hpp"
 #include "Graphics/Camera.hpp"
 
-Window_t Window {"Orbit", {1920, 1080}, {1920, 1080}};
-World_t World {{1920, 1080}, {1920, 1080}};
+Window_t Window {"Orbit", {1920, 1080}, {2560, 1440}};
+World_t World {{1920, 1080}, {2560, 1440}};
 Level_t Level;
 
 Game::Game()
