3897498926139735a65be4798819e57d4f3bb1ee
priyamkalra0 <priyamkalra0@gmail.com>
2025-11-23 03:03:00 +0530

    math/random: converted into header-only
    To avoid linker errors because of the templating

--- include/Math/Random.hpp
+++ include/Math/Random.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <random>
+#include <utility>
+#include "Math/Vector2.hpp"
 
 template <typename T>
 using urd = std::uniform_real_distribution<T>;
@@ -8,14 +10,33 @@ using urd = std::uniform_real_distribution<T>;
 class Random
 {
 public:
-    explicit Random(uint32_t seed);
-    Random();
+    explicit Random(uint32_t const seed)
+        : m_engine(seed) {}
 
-    template <class T>
-    T get(urd<T> const& dist);
+    Random()
+        : m_engine(std::random_device{}()) {}
 
-    template <class T>
-    T get(T const& min, T const& max);
+    template <typename T>
+    T get(urd<T> const& dist)
+    {
+        auto& _dist = const_cast<urd<T>&>(dist); /* Bad idea? */
+        return _dist(m_engine);
+    }
+
+    template <typename T>
+    T get(T const& min, T const& max)
+    {
+        return get(urd<T>{min, max});
+    }
+
+    template <>
+    sf::Vector2f get(sf::Vector2f const& min, sf::Vector2f const& max)
+    {
+        return {
+            get<float>(min.x, max.x),
+            get<float>(min.y, max.y)
+        };
+    }
 
 private:
     std::mt19937 m_engine;
