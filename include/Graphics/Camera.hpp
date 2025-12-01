198d80bdab285121f64540e7a02db8c34efcc097
priyamkalra0 <priyamkalra0@gmail.com>
2025-12-01 16:26:23 +0530

    graphics/camera: allow follow smoothing power to be adjusted at runtime

--- include/Graphics/Camera.hpp
+++ include/Graphics/Camera.hpp
@@ -9,6 +9,7 @@ public:
     Camera() = default;
 
     constexpr static float param_player_follow_smoothing_power { 0.98f };
+    constexpr static float param_seek_follow_smoothing_power { 0.99f };
 
     void update() const
     {
@@ -17,7 +18,7 @@ public:
         sf::Vector2f const new_pos {
                 current_pos +
                 (m_target - current_pos)
-                * (1.0f - param_player_follow_smoothing_power)
+                * (1.0f - m_follow_smoothing_power)
         };
 
         view.setCenter(new_pos);
@@ -26,12 +27,16 @@ public:
     void set_target(sf::Vector2f const& target) { if (!m_locked) m_target = target; }
     [[nodiscard]] sf::Vector2f const& get_target() const { return m_target; }
 
+    void set_follow_smoothing_power(float const power) { if (!m_locked) m_follow_smoothing_power = power; }
+    [[nodiscard]] float get_follow_smoothing_power() const { return m_follow_smoothing_power; }
+
     void unlock() { m_locked = false; }
     void lock() { m_locked = true; }
 
     [[nodiscard]] bool is_locked() const { return m_locked; }
 
 private:
+    float m_follow_smoothing_power { param_player_follow_smoothing_power };
     bool m_locked { false };
     sf::Vector2f m_target;
 };
